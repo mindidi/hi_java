b28ef4c345dd86d0e93329aa3bc5ff34ba00f20b
mindidi <alswlgg1@naver.com>
2023-08-07 12:43:32 +0900

    key

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -6,6 +6,7 @@ public class Hi_java {
 		 System.out.println("Hello");
 		 System.out.println("성공했네~~~~기쁘네 다행이네");
 		 System.out.println("성공유지!! ");
+		 System.out.println("key_업로드 안되게 밖으로");
 	}
 
 }
